529bcdce99b023b0ca4ea3ebaf99f772fcaff81f
cthdarren <ctze.darren@gmail.com>
2024-06-25 22:42:57 +0800

    scaffolded devise files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-         :authentication_keys =>[:number]
 
   has_one_attached :avatar 
 
