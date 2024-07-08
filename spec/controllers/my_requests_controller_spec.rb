7c2660bb0b2de533d0af05ffcd0837ababad2907
cthdarren <ctze.darren@gmail.com>
2024-07-08 14:53:04 +0800

    updated controller file definitions

--- spec/controllers/my_requests_controller_spec.rb
+++ spec/controllers/my_requests_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "MyRequestsControllers", type: :request do
+RSpec.describe "MyRequestsController", type: :controller do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
