2332ded16d521584b6a390ef6f592f37e87caed8
cthdarren <ctze.darren@gmail.com>
2024-07-09 19:42:34 +0800

    temp commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,37 +36,29 @@ RSpec.describe User, type: :model do
   end
   it 'has a profile picture attached' do
     # Save the subject to ensure it has an ID
-    subject.save
-    lmao = {
-      name: 'Testing User',
-      nric: 'T1234567J',
-      email: Faker::Internet.email,
-      number: "9#{Faker::Number.number(digits: 7)}",
-      status: 'Active',
-      role_id: 1,
-      password: 'password',
-      password_confirmation: 'password',
-      created_at: DateTime.now,
-      updated_at: DateTime.now
-    }
+    subject.save!
+    puts User.last.name
+    puts User.last.created_at
 
-    test = User.new(lmao)
-    test.save
     image_path = Rails.root.join('app', 'assets', 'images', 'default-avatar.png')
     image_file = File.open(image_path)
-    test.avatar.attach(io: image_file, filename: 'foo.png')
+    subject.avatar.attach(io: image_file, filename: 'foo.png')
+
     image_file.close
     puts 'Saved'
-    puts User.last.name
+    Rails.logger.info(subject.errors.inspect)
 
+    expect(subject.name).to eq('Testing User')
 
-    puts User.last.name
-    # Attach the file to the saved subject
-    # puts test.avatar.attached?
-
+    # puts User.last.name
+    # puts User.last.created_at
     # puts User.last.avatar.attached?
-    # puts "Errors: #{subject.errors.full_messages}"
-    # # Ensure the attachment is successful
-    # expect(subject.avatar.attached?).to eq(true)
+    # # Attach the file to the saved subject
+    # # puts test.avatar.attached?
+    #
+    # # puts User.last.avatar.attached?
+    # # puts "Errors: #{subject.errors.full_messages}"
+    # # # Ensure the attachment is successful
+    # expect(test.avatar.attached?).to eq(true)
   end
 end
