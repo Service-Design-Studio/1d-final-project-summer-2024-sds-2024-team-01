3e5ec149b3a55011951592d607dd4b9b138cd956
Amos <amosaurora@gmail.com>
2024-07-09 20:39:22 +0800

    edit reviews

---
+++ features/step_definitions/general_steps.rb
@@ -0,0 +1,43 @@
+Given('I am logged in') do
+  @user = User.find_or_create_by!(number: '123') do |user|
+    user.name = 'Evan Green'
+    user.nric = 'S5678901E'
+    user.email = 'evan.green@example.com'
+    user.status = 'Active'
+    user.role_id = 1
+    user.password = 'asdfasdf'
+    user.password_confirmation = 'asdfasdf'
+    user.created_at = DateTime.now
+    user.updated_at = DateTime.now
+  end
+
+  visit new_user_session_path
+  fill_in 'user_number', with: '123'
+  fill_in 'user_password', with: 'asdfasdf'
+  click_button 'Login'
+end
+
+Given('I am on the {string} page') do |page|
+  puts page
+  if page == 'home'
+    visit '/'
+  else
+    visit page
+  end
+end
+
+When('I click on {string} button') do |button|
+  click_button button
+end
+
+And('I press {string}') do |button_text|
+  click_button button_text
+end
+
+When('I follow {string}') do |link_text|
+  click_link link_text
+end
+
+Then('I should see {string}') do |message|
+  expect(page).to have_content(message)
+end
---
+++ features/step_definitions/reviews.rb
@@ -0,0 +1,55 @@
+When('I follow {string}') do |link_text|
+  click_link link_text
+end
+
+# And('I fill in {string} with {string}') do |field, value|
+#   fill_in field, with: value
+# end
+
+And('I press {string}') do |button|
+  click_button button
+end
+
+Then('I should see {string}') do |content|
+  expect(page).to have_content(content)
+end
+
+# Scenario: Leave a review for a user
+Given('there is a user named {string}') do |name|
+  @reviewee = User.create!(name: name, email: 'john@example.com', password: 'password')
+end
+
+When('I go to the new review page for {string}') do |name|
+  @reviewee = User.find_by(name: name)
+  visit new_user_review_path(@reviewee)
+end
+
+# Scenario: Edit a review
+Given('I have written a review for {string} with {string} and rating {string}') do |name, comment, rating|
+  @reviewee = User.find_by(name: name)
+  Review.create(review_for: @reviewee, created_by: @current_user, review_content: comment, rating: rating)
+end
+
+When('I click on {string} for the review') do |link_text|
+  link_to link_text
+end
+
+And('I change the rating in {string} to {string}') do |rating, new|
+  fill_in rating, with: new
+end
+
+And('I change the text in {string} to {string}') do |field, value|
+  fill_in field, with: value
+end
+
+# And('I press {string}') do |button|
+#   click_button button
+# end
+
+Then('I should see {string}') do |content|
+  expect(page).to have_content(content)
+end
+
+And('I should see "Updated review!"') do
+  p "Updated review!"
+end
