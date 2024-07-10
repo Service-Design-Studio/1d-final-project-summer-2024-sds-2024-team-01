830fca4b075ccbac6ab566adc111d0848340aac9
cthdarren <ctze.darren@gmail.com>
2024-07-10 09:38:08 +0800

    made some progress on myrequests ui

--- features/step_definitions/requestapplications.rb
+++ features/step_definitions/requestapplications.rb
@@ -1,64 +1,80 @@
 Given('I click on the header of the first request') do
+  pending
   # Add the code to click on the header of the first request
 end
 
 Given('the first request is fully filled') do
+  pending
   # Add the code to check if the first request is fully filled
 end
 
 Then('I should not be able to accept any applicants') do
+  pending
   # Add the code to verify that no applicants can be accepted
 end
 
 When('I reject the first applicant') do
   # Add the code to reject the first applicant
+  pending
 end
 
 Given('there is at least one request') do
   # Add the code to ensure there is at least one request
+  pending
 end
 
 When('I click on the first request') do
   # Add the code to click on the first request
+  pending
 end
 
 When('I click on Apply') do
   # Add the code to click on the Apply button
+  pending
 end
 
 When('I apply for a request') do
   # Add the code to apply for a request
+  pending
 end
 
 Then('I should see {string} in the status column of the most recent request') do |status|
   # Add the code to check the status of the most recent request
+  pending
 end
 
 When('my application has been accepted') do
   # Add the code to handle the acceptance of the application
+  pending
 end
 
 When('my application has been rejected') do
   # Add the code to handle the rejection of the application
+  pending
 end
 
 Then('I should see {string} under the status column of the first request') do |status|
   # Add the code to check the status under the first request
+  pending
 end
 
 Then('I should see the applicants who have applied for the request') do
   # Add the code to verify the list of applicants for the request
+  pending
 end
 
 Then('I should see the rating of each applicant') do
   # Add the code to verify the rating of each applicant
+  pending
 end
 
 When('I click on the profile section of the first applicant') do
   # Add the code to click on the profile section of the first applicant
+  pending
 end
 
 Then('I should see the applicants profile') do
   # Add the code to verify the applicant's profile
+  pending
 end
 
