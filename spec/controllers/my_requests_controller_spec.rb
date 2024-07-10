c9694d2d08e863ef59415cef139dc0cff3bf1f50
cthdarren <ctze.darren@gmail.com>
2024-07-11 06:09:51 +0800

    scaffolded rspec for myrequets, im gonig to sleep fk it

--- spec/controllers/my_requests_controller_spec.rb
+++ spec/controllers/my_requests_controller_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "MyRequestsController", type: :controller do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "Accepting a request" do
+    before do
+      
+    end
+    context "When accepting an application" do
+      it "updates it's status from pending to accepted"
+        expect()
+    end
   end
 end
