f96768b39a6fd3c3a54ea733c010da25cb6f50c6
Stormysearious <allison_yee@mymail.sutd.edu.sg>
2024-07-11 15:24:15 +0800

    not done rspec test

--- spec/controllers/my_applications_controller_spec.rb
+++ spec/controllers/my_applications_controller_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "MyApplicationsController", type: :controller do
+RSpec.describe MyApplicationsController, type: :controller do
+
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "returns a successful response" do
+      get :index
+      expect(response).to be_successful
+    end
+
+    it "returns an unsuccessful response" do
+      allow(controller).to receive(:index).and_raise(StandardError)
+      expect { get :index }.to raise_error(StandardError)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns a successful response" do
+      get :show, params: { id: request_application.id }
+      expect(response).to be_successful
+    end
+
+    it "returns an unsuccessful response" do
+      expect { get :show, params: { id: 0 } }.to raise_error(ActiveRecord::RecordNotFound)
+    end
   end
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe Request, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  subject {
+    described_class.new(
+      title: "Beach Cleanup",
+      category: "Environment",
+      location: "East Coast Park",
+      date: Date.today,
+      number_of_pax: 10,
+      duration: "2 hours",
+      reward_type: "Volunteer Points",
+      status: "Open",
+      created_by: user.id,
+      created_at: DateTime.now,
+      updated_at: DateTime.now
+    )
+  }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a title" do
+      subject.title = nil
+      subject.valid?
+      expect(subject.errors[:title]).to include("Please enter a title")
+    end
+
+    it "is not valid without a category" do
+      subject.category = nil
+      subject.valid?
+      expect(subject.errors[:category]).to include("Please select a category")
+    end
+
+    it "is not valid without a location" do
+      subject.location = nil
+      subject.valid?
+      expect(subject.errors[:location]).to include("Please enter a location")
+    end
+
+    it "is not valid without a date" do
+      subject.date = nil
+      subject.valid?
+      expect(subject.errors[:date]).to include("Please enter a date")
+    end
+
+    it "is not valid without a number_of_pax" do
+      subject.number_of_pax = nil
+      subject.valid?
+      expect(subject.errors[:number_of_pax]).to include("Please enter the number of people required")
+    end
+
+    it "is not valid without a duration" do
+      subject.duration = nil
+      subject.valid?
+      expect(subject.errors[:duration]).to include("Please enter a duration")
+    end
+
+    it "is not valid without a reward_type" do
+      subject.reward_type = nil
+      subject.valid?
+      expect(subject.errors[:reward_type]).to include("Please select a reward type")
+    end
+
+    it "is not valid without a status" do
+      subject.status = nil
+      subject.valid?
+      expect(subject.errors[:status]).to be_present
+    end
+
+    it "is not valid without a created_by" do
+      subject.created_by = nil
+      subject.valid?
+      expect(subject.errors[:created_by]).to be_present
+    end
+  end
 end
