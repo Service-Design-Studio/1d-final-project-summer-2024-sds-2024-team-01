a2998d8e189b273d4807cba56d8b342e8139736a
cthdarren <ctze.darren@gmail.com>
2024-07-11 10:04:12 +0800

    playing around with rspec

--- spec/controllers/my_requests_controller_spec.rb
+++ spec/controllers/my_requests_controller_spec.rb
@@ -1,13 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "MyRequestsController", type: :controller do
-  describe "Accepting a request" do
+RSpec.describe 'MyRequestsController', type: :controller do
+  let(:role) { create(:role) }
+  let(:requester) { create(:requester) }
+  let(:request) { create(:test_request, created_by: requester) }
+
+  describe 'Marking a request as complete' do
     before do
-      
+      sign_in requester
     end
-    context "When accepting an application" do
-      it "updates it's status from pending to accepted"
-        expect()
+
+    context 'When marking a request as complete' do
+      it "updates it's status from pending to complete" do
+        post :complete, params: { id: request.id }
+      end
+      it 'is now marked as complete' do
+        expect(request.status).to eq('complete')
+      end
     end
   end
+
+  #   describe 'Accepting a request' do
+  #     before do
+  #     end
+  #     context 'When accepting an application' do
+  #       it "updates it's status from pending to accepted"
+  #       expect
+  #     end
+  #   end
+  #
+  #   describe 'Accepting a request' do
+  #     before do
+  #     end
+  #     context 'When accepting an application' do
+  #       it "updates it's status from pending to accepted"
+  #       expect
+  #     end
+  #   end
 end
