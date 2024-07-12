d992909d0376e5fa3549492c1a657f0776b23af8
cthdarren <ctze.darren@gmail.com>
2024-07-12 12:28:52 +0800

    added rspec for charities

--- spec/models/charity_spec.rb
+++ spec/models/charity_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Charity, type: :model do
+  subject do
+    create(:random_charity)
+  end
+
+  it 'is not valid without a name' do
+    subject.charity_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a code' do
+    subject.charity_code = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a status' do
+    subject.status = nil
+    expect(subject).to_not be_valid
+  end
 end
