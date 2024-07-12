978d667432c4d445e4790f1f4760bb9e49eef59a
cthdarren <ctze.darren@gmail.com>
2024-07-12 15:04:55 +0800

    scaffolded some tests for the controller, will work on it some other days

--- app/controllers/my_devise/sessions_controller.rb
+++ app/controllers/my_devise/sessions_controller.rb
@@ -19,9 +19,7 @@ class MyDevise::SessionsController < DeviseController
   def create
     puts(auth_options)
     self.resource = warden.authenticate!(auth_options)
-    puts ("signing in")
     set_flash_message!(:notice, :signed_in)
-    puts ("signing in")
     sign_in(resource_name, resource)
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,7 @@ SimpleCov.start 'rails' do
     add_filter 'app/mailers'
     add_filter 'app/jobs'
     add_filter 'app/channels'
+    add_filter 'app/controllers/my_devise'
 end
 
 require 'cucumber/rails'
