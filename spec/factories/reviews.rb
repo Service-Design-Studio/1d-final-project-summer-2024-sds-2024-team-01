b0b9c7b7cf2e0ec57e21bfbeb0f137d76200ece0
cthdarren <ctze.darren@gmail.com>
2024-07-12 13:08:52 +0800

    fixed and finished review rspec

---
+++ spec/factories/reviews.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :random_review, class: 'Review' do
+    rating { Faker::Number.between(from: 1, to: 5) }
+    review_content { Faker::Quote.famous_last_words }
+    association :request, factory: :test_request, strategy: :build
+    association :review_for, factory: :random_user, strategy: :build
+    association :review_by, factory: :random_user, strategy: :build
+    created_at { DateTime.now }
+    updated_at { DateTime.now }
+  end
+end
