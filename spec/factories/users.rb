18e22bcec67082dc2d273996c1b64ce818d7d254
cthdarren <ctze.darren@gmail.com>
2024-07-13 22:27:46 +0800

    added script for making dummy data

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,7 +10,7 @@ FactoryBot.define do
   end
 
   factory :random_user, class: 'User' do
-    name { 'Random User' }
+    name { Faker::Name.name }
     email { Faker::Internet.email }
     number { "9#{Faker::Number.number(digits: 7)}" }
     status { 'Active' }
@@ -33,6 +33,36 @@ FactoryBot.define do
     name { 'Alice Smith' }
     number { "9#{Faker::Number.number(digits: 7)}" }
     email { 'alice.smith@example.com' }
+    status { 'Active' }
+    role_id { 1 }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
+
+  factory :dummy_user, class: 'User' do
+    name { 'Alice Smith' }
+    number { '91234567' }
+    email { 'alice.smith@example.com' }
+    status { 'Active' }
+    role_id { 1 }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
+
+  factory :dummy_user_two, class: 'User' do
+    name { 'Jane Doe' }
+    number { '81234567' }
+    email { 'jane.doe@example.com' }
+    status { 'Active' }
+    role_id { 1 }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
+
+  factory :dummy_user_three, class: 'User' do
+    name { 'Bob Dylan' }
+    number { '98765432' }
+    email { 'bob.dylan@example.com' }
     status { 'Active' }
     role_id { 1 }
     password { 'password' }
