a9d4983929fa39fc233cf639182dcb24e9aede0e
skywillow <delphine.sim.yingting@gmail.com>
2024-07-13 23:55:16 +0800

    idk what i changed

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,18 +2,12 @@
 # require "net/http"
 
 class ProfileController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index]
   def index
-    if params[:id].nil?
-      if current_user.nil?
-        render '/login'
-      else
-        @profile = current_user
-      end
-    else
-      @profile = User.find(params[:id])
-    end
-
+    @profile = if params[:id].nil?
+                 current_user
+               else
+                 User.find(params[:id])
+               end
     @requests = Request.where(created_by: @profile.id)
     @reviews_received = Review.where(review_for: @profile.id)
     @average_rating = @reviews_received.average(:rating)
