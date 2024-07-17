984aebb2c84e89d793e325baa2f081541f3929f9
cthdarren <ctze.darren@gmail.com>
2024-07-17 15:15:35 +0800

    fixed bug in cucumber signup tests, changed requests to not include thumbnails to reduce resources used

--- spec/factories/requests.rb
+++ spec/factories/requests.rb
@@ -3,7 +3,49 @@ FactoryBot.define do
     title { "Help with #{Faker::Verb.ing_form} #{Faker::Hacker.noun}" }
     description { Faker::Quote.fortune_cookie }
     category { 'General' }
-    location { "POINT(#{Faker::Number.between(from: 103.8, to: 104.0).round(3)} #{Faker::Number.between(from: 1.30, to: 1.40).round(3)})" }
+    location do
+      "POINT(#{Faker::Number.between(from: 103.8,
+                                     to: 104.0).round(3)} #{Faker::Number.between(
+                                       from: 1.30, to: 1.40
+                                     ).round(3)})"
+    end
+    date { Faker::Date.forward(from: Date.tomorrow, days: 90) }
+    start_time { '10:00 AM' }
+    number_of_pax { Faker::Number.between(from: 1, to: 10) }
+    duration { 2 }
+    reward_type { 'None' }
+    reward { 'None' }
+    status { 'Available' }
+    created_by { create(:random_user).id }
+    created_at { DateTime.now }
+    updated_at { DateTime.now }
+  end
+
+  factory :test_request, class: 'Request' do
+    title { 'Test Request' }
+    description { 'Need someone to walk my dog for an hour every afternoon' }
+    category { 'Pet Care' }
+    location { 'POINT(34.052235 -118.243683)' }
+    date { Date.tomorrow }
+    number_of_pax { 1 }
+    duration { 1 }
+    start_time { '12:00' }
+    reward { '$20' }
+    reward_type { 'Cash' }
+    status { 'Open' }
+    association :user, factory: :random_user, strategy: :build
+  end
+
+  factory :requestwiththumbnail do
+    title { "Help with #{Faker::Verb.ing_form} #{Faker::Hacker.noun}" }
+    description { Faker::Quote.fortune_cookie }
+    category { 'General' }
+    location do
+      "POINT(#{Faker::Number.between(from: 103.8,
+                                     to: 104.0).round(3)} #{Faker::Number.between(
+                                       from: 1.30, to: 1.40
+                                     ).round(3)})"
+    end
     date { Faker::Date.forward(from: Date.tomorrow, days: 90) }
     start_time { '10:00 AM' }
     number_of_pax { Faker::Number.between(from: 1, to: 10) }
@@ -24,20 +66,5 @@ FactoryBot.define do
     #   image_path =       request.thumbnail.attach(
     #   )
     # end
-  end
-
-  factory :test_request, class: 'Request' do
-    title { 'Test Request' }
-    description { 'Need someone to walk my dog for an hour every afternoon' }
-    category { 'Pet Care' }
-    location { 'POINT(34.052235 -118.243683)' }
-    date { Date.tomorrow }
-    number_of_pax { 1 }
-    duration { 1 }
-    start_time { '12:00' }
-    reward { '$20' }
-    reward_type { 'Cash' }
-    status { 'Open' }
-    association :user, factory: :random_user, strategy: :build
   end
 end
