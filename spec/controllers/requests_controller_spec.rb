3bc260fb2a379c6eaae1bdfd995e90f6f8c33354
cthdarren <ctze.darren@gmail.com>
2024-07-18 12:04:23 +0800

    covered new line in rspec

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -5,164 +5,178 @@ require 'rails_helper'
 RSpec.describe RequestsController, type: :controller do
   let(:testfile) { fixture_file_upload('app/assets/images/freepik-lmao.jpg', 'image/jpeg') }
   let(:user) { create(:user) }
+  let(:corp) { create(:user, role_id: 4) }
   let(:valid_attributes) { attributes_for(:request, created_by: user.id, thumbnail: testfile) }
   let(:valid_attributes_no_thumb) { attributes_for(:request, created_by: user.id) }
   let(:invalid_attributes) { attributes_for(:request, title: nil, created_by: user.id) }
 
-  before do
-    sign_in user
-  end
-
-  describe 'GET #index' do
-    it 'assigns @requests with requests to user ' do
-      request = create(:test_request, created_by: user.id)
-      get :index
-      expect(assigns(:requests)).to be_present
+  context 'corporate user' do
+    before do
+      sign_in corp
     end
-  end
-
-  describe 'GET #show' do
-    # alr have request created
-    let(:request) { create(:request, created_by: user.id) }
-
-    it 'returns a success response' do
-      get :show, params: { id: request.id }
-    end
-  end
-
-  describe 'GET #new' do
-    it 'returns a success response' do
-      get :new
-      expect(response).to be_successful
-    end
-  end
-
-  describe 'POST #create' do
-    context 'with valid params ' do
-      it 'creates a new Request with a thumbnail image' do
-        expect do
-          post :create, params: { request: valid_attributes }
-        end.to change(Request, :count).by(1)
-        expect(response).to redirect_to(Request.last)
-        expect(flash[:notice]).to eq('Request was successfully created.')
-        expect(Request.last.thumbnail).to be_attached
-      end
-
-      it 'creates a new request without a thumbnail' do
-        expect do
-          post :create, params: { request: valid_attributes_no_thumb }
-        end.to change(Request, :count).by(1)
-        expect(response).to redirect_to(Request.last)
-        expect(flash[:notice]).to eq('Request was successfully created.')
-        expect(Request.last.thumbnail).to be_attached
-      end
-    end
-
-    context 'with invalid params' do
-      it 'renders the new template with unprocessable entity status' do
-        post :create, params: { request: invalid_attributes }
-        expect(response).to render_template(:new)
-        expect(response.status).to eq(422)
+    describe 'GET #index' do
+      it 'assigns @requests with requests to user ' do
+        request = create(:request)
+        get :index
+        expect(assigns(:requests)).to be_present
       end
     end
   end
-
-  describe 'GET #edit' do
-    let(:request) { create(:request, created_by: user.id) }
-
-    it 'returns a success response' do
-      get :edit, params: { id: request.id }
-      expect(response).to be_successful
+  context 'normal user' do
+    before do
+      sign_in user
     end
-  end
+    describe 'GET #index' do
+      it 'assigns @requests with requests to user ' do
+        request = create(:test_request, created_by: user.id)
+        get :index
+        expect(assigns(:requests)).to be_present
+      end
+    end
 
-  # describe 'PATCH/PUT #update' do
-  #   let(:request) { create(:request, created_by: user.id) }
-  #   let(:new_attributes) { { title: 'Updated Title' } }
-  #
-  #   context 'with valid params' do
-  #     it 'updates the requested request' do
-  #       patch :update, params: { id: request.id, request: new_attributes }
-  #       request.reload
-  #       expect(request.title).to eq('Updated Title')
-  #     end
-  #
-  #     it 'redirects to the request' do
-  #       patch :update, params: { id: request.id, request: new_attributes }
-  #       expect(response).to redirect_to(request)
-  #     end
-  #   end
-  #
-  #   context 'with invalid params' do
-  #     it 'renders the edit template with unprocessable entity status' do
-  #       patch :update, params: { id: request.id, request: { title: nil } }
-  #       expect(response).to render_template(:edit)
-  #       expect(response.status).to eq(422)
-  #     end
-  #   end
-  # end
+    describe 'GET #show' do
+      # alr have request created
+      let(:request) { create(:request, created_by: user.id) }
 
-  describe 'POST #apply' do
-    let(:request) { create(:request) }
+      it 'returns a success response' do
+        get :show, params: { id: request.id }
+      end
+    end
 
-    context 'when all the params are not fulfilled somehow' do
-      it 'fails to save the application' do
-        allow_any_instance_of(RequestApplication).to receive(:save).and_return(false)
+    describe 'GET #new' do
+      it 'returns a success response' do
+        get :new
+        expect(response).to be_successful
+      end
+    end
 
-        expect do
+    describe 'POST #create' do
+      context 'with valid params ' do
+        it 'creates a new Request with a thumbnail image' do
+          expect do
+            post :create, params: { request: valid_attributes }
+          end.to change(Request, :count).by(1)
+          expect(response).to redirect_to(Request.last)
+          expect(flash[:notice]).to eq('Request was successfully created.')
+          expect(Request.last.thumbnail).to be_attached
+        end
+
+        it 'creates a new request without a thumbnail' do
+          expect do
+            post :create, params: { request: valid_attributes_no_thumb }
+          end.to change(Request, :count).by(1)
+          expect(response).to redirect_to(Request.last)
+          expect(flash[:notice]).to eq('Request was successfully created.')
+          expect(Request.last.thumbnail).to be_attached
+        end
+      end
+
+      context 'with invalid params' do
+        it 'renders the new template with unprocessable entity status' do
+          post :create, params: { request: invalid_attributes }
+          expect(response).to render_template(:new)
+          expect(response.status).to eq(422)
+        end
+      end
+    end
+
+    describe 'GET #edit' do
+      let(:request) { create(:request, created_by: user.id) }
+
+      it 'returns a success response' do
+        get :edit, params: { id: request.id }
+        expect(response).to be_successful
+      end
+    end
+
+    # describe 'PATCH/PUT #update' do
+    #   let(:request) { create(:request, created_by: user.id) }
+    #   let(:new_attributes) { { title: 'Updated Title' } }
+    #
+    #   context 'with valid params' do
+    #     it 'updates the requested request' do
+    #       patch :update, params: { id: request.id, request: new_attributes }
+    #       request.reload
+    #       expect(request.title).to eq('Updated Title')
+    #     end
+    #
+    #     it 'redirects to the request' do
+    #       patch :update, params: { id: request.id, request: new_attributes }
+    #       expect(response).to redirect_to(request)
+    #     end
+    #   end
+    #
+    #   context 'with invalid params' do
+    #     it 'renders the edit template with unprocessable entity status' do
+    #       patch :update, params: { id: request.id, request: { title: nil } }
+    #       expect(response).to render_template(:edit)
+    #       expect(response.status).to eq(422)
+    #     end
+    #   end
+    # end
+
+    describe 'POST #apply' do
+      let(:request) { create(:request) }
+
+      context 'when all the params are not fulfilled somehow' do
+        it 'fails to save the application' do
+          allow_any_instance_of(RequestApplication).to receive(:save).and_return(false)
+
+          expect do
+            post :apply, params: { id: request.id }
+          end.to change(RequestApplication, :count).by(0)
+        end
+      end
+
+      context 'when the user has not applied before' do
+        it 'creates a new RequestApplication' do
+          expect do
+            post :apply, params: { id: request.id }
+          end.to change(RequestApplication, :count).by(1)
+          expect(RequestApplication.last.applicant_id).to eq(user.id)
+          expect(RequestApplication.last.request_id).to eq(request.id)
+        end
+
+        it 'redirects to the request with a success notice' do
           post :apply, params: { id: request.id }
-        end.to change(RequestApplication, :count).by(0)
+          expect(response).to redirect_to(request)
+          expect(flash[:notice]).to eq('Successfully applied for the request.')
+        end
       end
-    end
 
-    context 'when the user has not applied before' do
-      it 'creates a new RequestApplication' do
-        expect do
+      context 'when the user has already applied' do
+        before do
+          create(:application, applicant_id: user.id, request_id: request.id)
+        end
+
+        it 'does not create a new RequestApplication' do
+          expect do
+            post :apply, params: { id: request.id }
+          end.not_to change(RequestApplication, :count)
+        end
+
+        it 'redirects to the request with an alert' do
           post :apply, params: { id: request.id }
-        end.to change(RequestApplication, :count).by(1)
-        expect(RequestApplication.last.applicant_id).to eq(user.id)
-        expect(RequestApplication.last.request_id).to eq(request.id)
-      end
-
-      it 'redirects to the request with a success notice' do
-        post :apply, params: { id: request.id }
-        expect(response).to redirect_to(request)
-        expect(flash[:notice]).to eq('Successfully applied for the request.')
+          expect(response).to redirect_to(request)
+          expect(flash[:alert]).to eq('You have already applied for this request.')
+        end
       end
     end
 
-    context 'when the user has already applied' do
-      before do
-        create(:application, applicant_id: user.id, request_id: request.id)
-      end
+    describe 'DELETE #destroy' do
+      let!(:request) { create(:request, created_by: user.id) }
 
-      it 'does not create a new RequestApplication' do
+      it 'destroys the requested request' do
         expect do
-          post :apply, params: { id: request.id }
-        end.not_to change(RequestApplication, :count)
+          delete :destroy, params: { id: request.id }
+        end.to change(Request, :count).by(-1)
       end
 
-      it 'redirects to the request with an alert' do
-        post :apply, params: { id: request.id }
-        expect(response).to redirect_to(request)
-        expect(flash[:alert]).to eq('You have already applied for this request.')
-      end
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    let!(:request) { create(:request, created_by: user.id) }
-
-    it 'destroys the requested request' do
-      expect do
+      it 'redirects to the requests list' do
         delete :destroy, params: { id: request.id }
-      end.to change(Request, :count).by(-1)
-    end
-
-    it 'redirects to the requests list' do
-      delete :destroy, params: { id: request.id }
-      expect(response).to redirect_to(requests_url)
-      expect(flash[:notice]).to eq('Request was successfully destroyed.')
+        expect(response).to redirect_to(requests_url)
+        expect(flash[:notice]).to eq('Request was successfully destroyed.')
+      end
     end
   end
 end
