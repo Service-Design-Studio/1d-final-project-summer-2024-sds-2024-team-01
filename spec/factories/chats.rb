10813743a01e91a2f049050931cb43bd27e13baf
cthdarren <ctze.darren@gmail.com>
2024-07-19 13:01:47 +0800

    navbar ui changes, fixed factories to use requestswiththumbnails instead

--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :random_chat, class: 'Chat' do
     association :applicant, factory: :random_user
     association :requester, factory: :random_user
-    association :request, factory: :request
+    association :request, factory: :requestwiththumbnail
   end
 end
