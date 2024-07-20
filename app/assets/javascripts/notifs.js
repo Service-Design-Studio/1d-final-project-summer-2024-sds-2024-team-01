4a793c17e96a7dcf09279c7ee711ce2f36edf51d
cthdarren <ctze.darren@gmail.com>
2024-07-20 15:33:48 +0800

    progress on notification ui

--- app/assets/javascripts/notifs.js
+++ app/assets/javascripts/notifs.js
@@ -1,3 +1,10 @@
-function togglenotifs(){
-    console.log("open!")
+function togglenotifs() {
+    console.log(document.getElementById("notificationpopup").style.display)
+    if (document.getElementById("notificationpopup").style.display == "block") {
+        console.log("close!");
+        document.getElementById("notificationpopup").style.display = "none";
+    } else {
+        document.getElementById("notificationpopup").style.display = "block";
+        console.log("open!");
+    }
 }
