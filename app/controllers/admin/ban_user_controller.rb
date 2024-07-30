df939d36810df2e63be4dcd9fc746276cc3b4631
Stormysearious <allison_yee@mymail.sutd.edu.sg>
2024-07-30 12:39:33 +0800

    banuser controller to add notification

--- app/controllers/admin/ban_user_controller.rb
+++ app/controllers/admin/ban_user_controller.rb
@@ -3,44 +3,44 @@ class Admin::BanUserController < ApplicationController
   before_action :authorize_admin!
 
   def index
-    @under_review_users = User.joins(:user_reports_as_reported_user).where(user_reports: { status: 'under_review' }).distinct
-    @banned_users = User.joins(:user_reports_as_reported_user).where(user_reports: { status: 'ban' }).distinct
+    @under_review_users = User.joins(:user_reports_as_reported_user).where(user_reports: { status: UserReport.statuses[:under_review] }).distinct
+    @banned_users = User.joins(:user_reports_as_reported_user).where(user_reports: { status: UserReport.statuses[:banned] }).distinct
   end
 
   def ban
     user = User.find(params[:id])
-    user_report = user.user_reports_as_reported_user.find_by(status: 'under_review')
-    if user_report && user_report.update(status: 'ban')
-      user.update(status: 'banned')
-      render json: { success: true }
+    user_report = user.user_reports_as_reported_user.find_by(status: UserReport.statuses[:under_review])
+    if user_report && user_report.update(status: :banned)
+      user.update(status: :banned)
+      Notification.create(user: user, message: 'Your account has been banned.')
+      render json: { success: true, message: 'User has been banned.' }
     else
-      render json: { success: false }
+      render json: { success: false, message: 'Failed to ban user.' }
     end
   end
 
   def unban
     user = User.find(params[:id])
-    user_report = user.user_reports_as_reported_user.find_by(status: 'ban')
-    if user_report && user_report.update(status: 'normal')
-      user.update(status: 'normal')
-      render json: { success: true }
+    user_report = user.user_reports_as_reported_user.find_by(status: :banned)
+    if user_report && user_report.update(status: :normal)
+      user.update(status: :normal)
+      Notification.create(user: user, message: 'Your account has been unbanned. Welcome back!')
+      render json: { success: true, message: 'User has been unbanned.' }
     else
-      render json: { success: false }
+      render json: { success: false, message: 'Failed to unban user.' }
     end
   end
 
   def cancel_ban
     user = User.find(params[:id])
-    user_report = user.user_reports_as_reported_user.find_by(status: 'under_review')
-    if user_report && user_report.update(status: 'normal')
-      user.update(status: 'normal')
-      render json: { success: true }
+    user_report = user.user_reports_as_reported_user.find_by(status: UserReport.statuses[:under_review])
+    if user_report && user_report.update(status: :normal)
+      user.update(status: :normal)
+      render json: { success: true, message: 'Ban has been cancelled.' }
     else
-      render json: { success: false }
+      render json: { success: false, message: 'Failed to cancel ban.' }
     end
   end
-
-
 
   private
 
