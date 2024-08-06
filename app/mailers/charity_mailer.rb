6e240406c109a51fdd02b7d1a49f1fc6d2824e82
Stormysearious <allison_yee@mymail.sutd.edu.sg>
2024-08-06 21:50:22 +0800

    add mailer test code

--- app/mailers/charity_mailer.rb
+++ app/mailers/charity_mailer.rb
@@ -1,10 +1,15 @@
 class CharityMailer < ApplicationMailer
-    default from: 'no-reply@example.com'  # Replace with your desired default sender email
-  
-    def approval_email
-      @charity = params[:charity]
-      @code = params[:code]
-      mail(to: @charity.users.first.email, subject: 'Your Charity has been Approved')
+  default from: 'no-reply@example.com'  # Replace with your desired default sender email
+
+  def approval_email
+    @charity = params[:charity]
+    @code = params[:code]
+    charity_manager = @charity.users.find_by(role_id: 5)
+    if charity_manager
+      Rails.logger.debug "Sending approval email to: #{charity_manager.email}"
+      mail(to: charity_manager.email, subject: 'Your Charity has been Approved')
+    else
+      Rails.logger.debug "No charity manager found for charity: #{@charity.id}"
     end
   end
-  
+end
--- spec/mailers/company_mailer_spec.rb
+++ spec/mailers/company_mailer_spec.rb
@@ -1,21 +1,25 @@
-# spec/mailers/company_mailer_spec.rb
 require "rails_helper"
 
 RSpec.describe CompanyMailer, type: :mailer do
   describe 'approval_email' do
-    let(:company) { create(:company, email: 'test@example.com') }
-    let(:code) { 'unique_code_123' }
-    let(:mail) { CompanyMailer.with(company: company, code: code).approval_email }
+    let(:company) { create(:random_company) }
+    let(:code) { 'unique_code' }
+    let(:mail) { described_class.with(company: company, code: code).approval_email.deliver_now }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('Your company has been approved')
-      expect(mail.to).to eq([company.email])
-      expect(mail.from).to eq(['from@example.com']) # Change to your default from email
+      expect(mail.subject).to eq('Your Company has been Approved')
+      expect(mail.to).to eq([company.users.find_by(role_id: 3).email])
+      expect(mail.from).to eq(['no-reply@example.com'])
     end
 
     it 'renders the body' do
-      expect(mail.body.encoded).to match('Your company has been approved')
-      expect(mail.body.encoded).to match(code)
+      # Check for specific parts of the HTML content
+      expect(mail.body.encoded).to include("Dear #{company.users.find_by(role_id: 3).name},")
+      expect(mail.body.encoded).to include("We are pleased to inform you that your company, #{company.company_name}, has been approved.")
+      expect(mail.body.encoded).to include("Your approval code is: #{code}")
+      expect(mail.body.encoded).to include("Thank you for your cooperation.")
+      expect(mail.body.encoded).to include("Best regards,<br>")
+      expect(mail.body.encoded).to include("The Team")
     end
   end
 end
