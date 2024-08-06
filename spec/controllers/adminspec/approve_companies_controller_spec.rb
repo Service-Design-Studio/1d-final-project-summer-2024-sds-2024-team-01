84193662943ff4271cf4e98b7037dd615275953d
Stormysearious <allison_yee@mymail.sutd.edu.sg>
2024-08-06 09:01:47 +0800

    spec passed

--- spec/controllers/adminspec/approve_companies_controller_spec.rb
+++ spec/controllers/adminspec/approve_companies_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Admin::ApproveCompaniesController, type: :controller do
   let(:admin) { create(:user, :admin) }
-  let(:company) { create(:company) }
+  let(:inactive_company) { create(:friendly_company) }
+  let(:active_company) { create(:love_company) }
+  let(:rejected_company) { create(:grace_company, status: 'Rejected') }
 
   before do
     sign_in admin
@@ -17,34 +19,34 @@ RSpec.describe Admin::ApproveCompaniesController, type: :controller do
 
   describe 'GET #show' do
     it 'returns a success response' do
-      get :show, params: { id: company.id }
+      get :show, params: { id: inactive_company.id }
       expect(response).to be_successful
     end
   end
 
   describe 'PATCH #approve' do
     it 'approves the company' do
-      patch :approve, params: { id: company.id }
-      company.reload
-      expect(company.status).to eq('Active')
+      patch :approve, params: { id: inactive_company.id }
+      inactive_company.reload
+      expect(inactive_company.status).to eq('Active')
       expect(response).to redirect_to(admin_approve_companies_path)
     end
   end
 
   describe 'PATCH #disable' do
     it 'disables the company' do
-      patch :disable, params: { id: company.id }
-      company.reload
-      expect(company.status).to eq('Inactive')
+      patch :disable, params: { id: active_company.id }
+      active_company.reload
+      expect(active_company.status).to eq('Inactive')
       expect(response).to redirect_to(admin_approve_companies_path)
     end
   end
 
   describe 'PATCH #reject' do
     it 'rejects the company' do
-      patch :reject, params: { id: company.id }
-      company.reload
-      expect(company.status).to eq('Rejected')
+      patch :reject, params: { id: inactive_company.id }
+      inactive_company.reload
+      expect(inactive_company.status).to eq('Rejected')
       expect(response).to redirect_to(admin_approve_companies_path)
     end
   end
